9dab4efb7f83c27ea38fcab1b504738b1ba7a7fe
carljin <buffpal2016@gmail.com>
2024-10-04 22:07:43 -0400

    feat: 按照模块更改进行替换，而不是替换所有模块

--- src/installer.js
+++ src/installer.js
@@ -10,7 +10,8 @@ const {
   writeFileSync,
   readFileSync,
 } = require("fs");
-const { join, resolve, basename } = require("node:path");
+const { join, resolve, basename } = require("path");
+const { dirname } = require("node:path");
 
 try {
   copyFolderRecursiveSync("__unzipPath__", "__appPath__");
@@ -42,13 +43,17 @@ function copyFolderRecursiveSync(source, target) {
   const files = readdirSync(source);
 
   // 遍历文件列表，处理每个文件或子目录
-  files.forEach((file) => {
-    const sourcePath = join(source, file);
-    const targetPath = join(target, file);
+  files.forEach((folderOrFile) => {
+    const sourcePath = join(source, folderOrFile);
+    const targetPath = join(target, folderOrFile);
 
     // 如果当前文件是文件夹，则递归复制文件夹
     if (statSync(sourcePath).isDirectory()) {
-      copyFolderRecursiveSync(sourcePath, targetPath);
+      if (folderOrFile === "node_modules") {
+        replaceChangedModuleSync(sourcePath, targetPath);
+      } else {
+        copyFolderRecursiveSync(sourcePath, targetPath);
+      }
     } else {
       // 否则，复制文件
       try {
@@ -79,4 +84,46 @@ function copyFileSync(source, target) {
   }
 
   writeFileSync(targetFile, readFileSync(source));
+}
+
+function replaceChangedModuleSync(
+  sourceNodeModulesFolder,
+  destNodeModulesFolder
+) {
+  //  读取 evdUnzip/node_modules 所有子文件
+  const fileOrFolder = readdirSync(sourceNodeModulesFolder);
+
+  // 遍历文件列表，处理每个文件或子目录
+  fileOrFolder.forEach((folderOrFileName) => {
+    const sourcePath = join(sourceNodeModulesFolder, folderOrFileName);
+    const targetPath = join(destNodeModulesFolder, folderOrFileName);
+
+    // 如果当前文件是文件夹，则判断是否要更新
+    if (statSync(sourcePath).isDirectory()) {
+      //  如果不存在，或者版本不一样则进行替换
+      if (shouldReplaceModule(sourcePath, targetPath)) {
+        copyFolderRecursiveSync(sourcePath, targetPath);
+      }
+    } else {
+      copyFileSync(sourcePath, targetPath);
+    }
+  });
+}
+
+function shouldReplaceModule(sourcePath, destPath) {
+  const sourceVersion = getPackageVersion(sourcePath);
+  const destVersion = getPackageVersion(destPath);
+  return sourceVersion !== destVersion;
+}
+
+async function getPackageVersion(packagePath) {
+  try {
+    const packageJsonPath = join(packagePath, "package.json");
+    const packageJsonContent = readFileSync(packageJsonPath, "utf-8");
+    const packageJson = JSON.parse(packageJsonContent);
+    return packageJson.version;
+  } catch (error) {
+    console.error(`Error reading package.json in ${packagePath}:`, error);
+    return null;
+  }
 }
